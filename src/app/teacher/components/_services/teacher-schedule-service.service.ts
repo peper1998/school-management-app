68453caa1457d8a248c0338065c4e0077e2fa9dc
patrykkimmel <patrykkimmel@gmail.com>
2020-09-05 10:26:10 +0200

    hotfix, scheduler installed and one function fixed

--- src/app/teacher/components/_services/teacher-schedule-service.service.ts
+++ src/app/teacher/components/_services/teacher-schedule-service.service.ts
@@ -3,18 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { teacherLessonsGet } from '../_models/teacherLessonsGet';
 import { User } from 'src/app/_models/user';
-import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
 //import { defaultMaxListeners } from 'stream';
-import { LessonNumber } from 'src/app/_enums/LessonNumber';
-import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherScheduleServiceService {
 
-  constructor(private http: HttpClient) 
-  { 
+  constructor(private http: HttpClient)
+  {
 
   }
   getLessonsTeacher(id:number):Observable<teacherLessonsGet[]>{
--- src/app/teacher/components/teacher-marks/teacher-marks/teacher-marks.component.ts
+++ src/app/teacher/components/teacher-marks/teacher-marks/teacher-marks.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ClassModel } from 'src/app/_models/class/class.model';
-import { TeacherCourseLinkingModel } from 'src/app/_models/courses/teacher-course-linking.model';
 import { entityClass } from '../../_models/entityClass';
 import { TeacherMarksService } from '../../_services/teacher-marks.service';
 import { teachersCoursesGet } from '../../_models/teacherCoursesGet';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { CourseModel } from 'src/app/_models/courses/course.model';
-import { StudentCreationModel } from 'src/app/_models/parents_students/student.model';
 import { studentDisplayGradesModel } from '../../_models/studentDisplayGradesModel';
 import { StudentMarksGetDTO, StudentMarksGetDTODisplay } from '../../_models/StudentMarksGetDTO';
 import { MarkPostDTO } from '../../_models/MarkPostDTO';
-import { NONE_TYPE } from '@angular/compiler';
 import { enumGrade } from '../../_models/enumGrade';
 const createFormGroup = dataItem => new FormGroup({
   'description': new FormControl(dataItem.description, Validators.required),
-  'grade': new FormControl(dataItem.grade, Validators.compose([Validators.required,Validators.pattern('[1-6]{1}')])),
+  'grade': new FormControl(dataItem.grade, Validators.compose([Validators.required, Validators.pattern('[1-6]{1}')])),
   'lastChange': new FormControl(dataItem.lastChange)
 });
 @Component({
@@ -24,59 +19,56 @@ const createFormGroup = dataItem => new FormGroup({
 })
 export class TeacherMarksComponent implements OnInit {
 
-  constructor(private teacherMarksService:TeacherMarksService) { }
-  srudentSelected:boolean = false;
-  selectedStudentObject:StudentMarksGetDTODisplay;
+  constructor(private teacherMarksService: TeacherMarksService) { }
+  srudentSelected: boolean = false;
+  selectedStudentObject: StudentMarksGetDTODisplay;
   ngOnInit(): void {
-    this.teacherMarksService.getTeacherClasses().subscribe(res=>{
-      this.classesList =res;
+    this.teacherMarksService.getTeacherClasses().subscribe(res => {
+      this.classesList = res;
       console.log(this.classesList);
     },
-    error=>{alert("couldnt fetch teacher classes")})
+      () => { alert("couldnt fetch teacher classes") })
   }
   @Input() classesList: entityClass[];
   @Input() teacherCoursesList: teachersCoursesGet[];
   @Input() studentsList: studentDisplayGradesModel[];
-  @Input() studentsMarkGet:StudentMarksGetDTO[];
+  @Input() studentsMarkGet: StudentMarksGetDTO[];
   @Output() dataStateChanged: EventEmitter<any> = new EventEmitter()
 
   public formGroup: FormGroup;
   public editedRowIndex: number;
 
-  private editMode:boolean;
   selectedClassId: Number;
   selectedTeacherCourseId: Number;
 
-  dropdownValueChanged(value:any)
-  {
+  dropdownValueChanged(value: any) {
     this.studentsList = null;
     this.selectedStudentObject = null;
-    this.selectedClassId =value.id;
-    this.teacherMarksService.getTeacherCoursesOfGivenClassById(value.id).subscribe(res=>{
-      this.teacherCoursesList =res;
-      this.teacherCoursesList.forEach(e=> e.displayName = e.course.name);
-    }, error=> { alert("Couldnt fectch teacher courses ")})
+    this.selectedClassId = value.id;
+    this.teacherMarksService.getTeacherCoursesOfGivenClassById(value.id).subscribe(res => {
+      this.teacherCoursesList = res;
+      this.teacherCoursesList.forEach(e => e.displayName = e.course.name);
+    }, () => { alert("Couldnt fectch teacher courses ") })
   }
 
-  teacherCourseDropdownValueChanged(value:any)
-  {
+  teacherCourseDropdownValueChanged(value: any) {
     this.studentsList = null;
     this.selectedStudentObject = null;
     this.selectedTeacherCourseId = value.id;
     console.log(this.selectedTeacherCourseId);
     console.log(this.selectedClassId);
-    this.teacherMarksService.getStudentsOfGivenClassById(this.selectedClassId).subscribe(res=>{
-      this.studentsList =res;
-      this.teacherMarksService.getStudentsGrades(this.selectedClassId,this.selectedTeacherCourseId).subscribe(res=>{
+    this.teacherMarksService.getStudentsOfGivenClassById(this.selectedClassId).subscribe(res => {
+      this.studentsList = res;
+      this.teacherMarksService.getStudentsGrades(this.selectedClassId, this.selectedTeacherCourseId).subscribe(res => {
         this.studentsMarkGet = res;
         console.log(this.studentsMarkGet);
         this.mapGradesToString();
-        this.studentsList.forEach(e=>console.log(e.grades));
-      }, error=>{alert("Couldnt fetch student marks")})
+        this.studentsList.forEach(e => console.log(e.grades));
+      }, () => { alert("Couldnt fetch student marks") })
       //this.teacherCoursesList.forEach(e=> e.displayName = e.course.name);
-    }, error=> { alert("Couldnt fetch stundents")})
+    }, () => { alert("Couldnt fetch stundents") })
   }
-  private mapGradeToStr= {
+  private mapGradeToStr = {
     NONE: "0",
     ONE: "1",
     TWO: "2",
@@ -85,9 +77,9 @@ export class TeacherMarksComponent implements OnInit {
     FIVE: "5",
     SIX: "6"
   }
-  private mapStrToGrade= {
+  private mapStrToGrade = {
     "0": enumGrade.NONE,
-    "1":enumGrade.ONE,
+    "1": enumGrade.ONE,
     "2": enumGrade.TWO,
     "3": enumGrade.THREE,
     "4": enumGrade.FOUR,
@@ -95,32 +87,43 @@ export class TeacherMarksComponent implements OnInit {
     "6": enumGrade.SIX
   }
 
-  selectStudent(value:any){
-    this.srudentSelected=!this.srudentSelected;
+  selectStudent(value: any) {
+    this.srudentSelected = !this.srudentSelected;
     //if(this.srudentSelected){
-    this.selectedStudentObject=<StudentMarksGetDTODisplay>this.studentsMarkGet[value.rowIndex];
-    this.selectedStudentObject.grades.forEach(e=>{ 
-      e.grade=this.mapGradeToStr[e.enumGrade];
+    this.selectedStudentObject = <StudentMarksGetDTODisplay>this.studentsMarkGet[value.rowIndex];
+    this.selectedStudentObject.grades.forEach(e => {
+      e.grade = this.mapGradeToStr[e.enumGrade];
       console.log(e.grade);
     });
     console.log(this.studentsMarkGet[value.rowIndex]);
-  //}
-    // else{ 
+    //}
+    // else{
     //   this.selectedStudentObject=null;
     // }
   }
   mapGradesToString() {
-    for(var i =0;i<this.studentsMarkGet.length;i++) {
-      for(var g=0;g<this.studentsMarkGet[i].grades?.length;g++){
-      this.studentsList[i].grades += this.mapGradeToStr[this.studentsMarkGet[i].grades[g].enumGrade] +', ';
-    }
-    this.studentsList[i].grades= this.studentsList[i].grades.slice(9,this.studentsList[i].grades.length- 2);
-    console.log(this.studentsList[i].grades);
-    }
+    let i = 0;
+    this.studentsMarkGet.forEach(m => {
+      let gradeIndex = 0;
+      m.grades.forEach(() => {
+        this.studentsList[i].grades += this.mapGradeToStr[this.studentsMarkGet[i].grades[gradeIndex].enumGrade] + ', ';
+        gradeIndex++;
+      })
+      i++;
+    })
+
+
+
+    // for (var i = 0; i < this.studentsMarkGet.length; i++) {
+    //   for (var g = 0; g < this.studentsMarkGet[i].grades?.length; g++) {
+    //     this.studentsList[i].grades += this.mapGradeToStr[this.studentsMarkGet[i].grades[g].enumGrade] + ', ';
+    //   }
+    //   this.studentsList[i].grades = this.studentsList[i].grades.slice(9, this.studentsList[i].grades.length - 2);
+    //   console.log(this.studentsList[i].grades);
+    // }
   }
   public editHandler({ sender, rowIndex, dataItem }) {
     this.closeEditor(sender);
-    this.editMode=true;
 
     this.formGroup = new FormGroup({
       'grade': new FormControl(dataItem.grade),
@@ -136,33 +139,31 @@ export class TeacherMarksComponent implements OnInit {
     this.closeEditor(sender, rowIndex);
   }
 
-  public addHandler({sender}) {
+  public addHandler({ sender }) {
     this.closeEditor(sender);
-    this.editMode=false;
 
     this.formGroup = createFormGroup({
-      'description':'',
-      'grade':1,
-      'lastChange':new Date()
+      'description': '',
+      'grade': 1,
+      'lastChange': new Date()
     })
 
     sender.addRow(this.formGroup);
-}
+  }
 
   public saveHandler({ sender, rowIndex, formGroup, isNew, dataItem }) {
     var markPostDTO = new MarkPostDTO();
-    markPostDTO.description=  formGroup.get("description").value;
+    markPostDTO.description = formGroup.get("description").value;
     markPostDTO.enumGrade = this.mapStrToGrade[formGroup.get("grade").value];
     markPostDTO.studentId = this.selectedStudentObject.id;
     markPostDTO.teacherCourseId = this.selectedTeacherCourseId;
-    if(isNew)
-    {
-      this.teacherMarksService.postNewMark(markPostDTO).subscribe(course=>{
+    if (isNew) {
+      this.teacherMarksService.postNewMark(markPostDTO).subscribe(course => {
         console.log(course);
         this.dataStateChanged.emit();
       });
-    }else{
-      this.teacherMarksService.editMark(markPostDTO,dataItem.id).subscribe(course=>{
+    } else {
+      this.teacherMarksService.editMark(markPostDTO, dataItem.id).subscribe(course => {
         this.dataStateChanged.emit();
         console.log(course);
       });;
@@ -172,21 +173,21 @@ export class TeacherMarksComponent implements OnInit {
     this.reflesh(rowIndex);
   }
   reflesh(rowIndex) {
-    this.teacherMarksService.getStudentsOfGivenClassById(this.selectedClassId).subscribe(res=>{
-      this.studentsList =res;
-      this.teacherMarksService.getStudentsGrades(this.selectedClassId,this.selectedTeacherCourseId).subscribe(res=>{
+    this.teacherMarksService.getStudentsOfGivenClassById(this.selectedClassId).subscribe(res => {
+      this.studentsList = res;
+      this.teacherMarksService.getStudentsGrades(this.selectedClassId, this.selectedTeacherCourseId).subscribe(res => {
         this.studentsMarkGet = res;
         console.log(this.studentsMarkGet);
         this.mapGradesToString();
-        this.studentsList.forEach(e=>console.log(e.grades));
-        this.selectedStudentObject=<StudentMarksGetDTODisplay>this.studentsMarkGet[rowIndex];
-        this.selectedStudentObject.grades.forEach(e=>{ 
-          e.grade=this.mapGradeToStr[e.enumGrade];
+        this.studentsList.forEach(e => console.log(e.grades));
+        this.selectedStudentObject = <StudentMarksGetDTODisplay>this.studentsMarkGet[rowIndex];
+        this.selectedStudentObject.grades.forEach(e => {
+          e.grade = this.mapGradeToStr[e.enumGrade];
           console.log(e.grade);
         });
-      }, error=>{alert("Couldnt fetch student marks")})
+      }, () => { alert("Couldnt fetch student marks") })
       //this.teacherCoursesList.forEach(e=> e.displayName = e.course.name);
-    }, error=> { alert("Couldnt fetch stundents")})
+    }, () => { alert("Couldnt fetch stundents") })
 
   }
   //   sender.closeRow(rowIndex);
@@ -204,7 +205,7 @@ export class TeacherMarksComponent implements OnInit {
     this.formGroup = undefined;
   }
   public removeHandler({ dataItem, rowIndex }) {
-    this.teacherMarksService.deleteMark(dataItem.id).subscribe(resp=>{
+    this.teacherMarksService.deleteMark(dataItem.id).subscribe(resp => {
       console.log(resp);
     });
     this.reflesh(rowIndex);
