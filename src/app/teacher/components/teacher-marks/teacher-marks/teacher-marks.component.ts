de6c1bd8d402ceccdaef055105b227e50fac49eb
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-09-05 13:42:38 +0200

    cleaning up the code, adding marks fully working

--- src/app/teacher/components/teacher-marks/teacher-marks/teacher-marks.component.ts
+++ src/app/teacher/components/teacher-marks/teacher-marks/teacher-marks.component.ts
@@ -37,7 +37,7 @@ export class TeacherMarksComponent implements OnInit {
 
   public formGroup: FormGroup;
   public editedRowIndex: number;
-
+  selectedStudentRowNumber:number;
   selectedClassId: Number;
   selectedTeacherCourseId: Number;
 
@@ -65,7 +65,6 @@ export class TeacherMarksComponent implements OnInit {
         this.mapGradesToString();
         this.studentsList.forEach(e => console.log(e.grades));
       }, () => { alert("Couldnt fetch student marks") })
-      //this.teacherCoursesList.forEach(e=> e.displayName = e.course.name);
     }, () => { alert("Couldnt fetch stundents") })
   }
   private mapGradeToStr = {
@@ -88,18 +87,13 @@ export class TeacherMarksComponent implements OnInit {
   }
 
   selectStudent(value: any) {
-    this.srudentSelected = !this.srudentSelected;
-    //if(this.srudentSelected){
+    this.selectedStudentRowNumber = value.rowIndex;
     this.selectedStudentObject = <StudentMarksGetDTODisplay>this.studentsMarkGet[value.rowIndex];
     this.selectedStudentObject.grades.forEach(e => {
       e.grade = this.mapGradeToStr[e.enumGrade];
       console.log(e.grade);
     });
     console.log(this.studentsMarkGet[value.rowIndex]);
-    //}
-    // else{
-    //   this.selectedStudentObject=null;
-    // }
   }
   mapGradesToString() {
     let i = 0;
@@ -119,27 +113,16 @@ export class TeacherMarksComponent implements OnInit {
       firstIteration=true;
       i++;
     })
-
-
-
-    // for (var i = 0; i < this.studentsMarkGet.length; i++) {
-    //   for (var g = 0; g < this.studentsMarkGet[i].grades?.length; g++) {
-    //     this.studentsList[i].grades += this.mapGradeToStr[this.studentsMarkGet[i].grades[g].enumGrade] + ', ';
-    //   }
-    //   this.studentsList[i].grades = this.studentsList[i].grades.slice(9, this.studentsList[i].grades.length - 2);
-    //   console.log(this.studentsList[i].grades);
-    // }
   }
+
   public editHandler({ sender, rowIndex, dataItem }) {
     this.closeEditor(sender);
-
     this.formGroup = new FormGroup({
       'grade': new FormControl(dataItem.grade),
       'description': new FormControl(dataItem.description, [Validators.required, Validators.minLength(3)]),
     });
 
     this.editedRowIndex = rowIndex;
-
     sender.editRow(rowIndex, this.formGroup);
   }
 
@@ -155,7 +138,6 @@ export class TeacherMarksComponent implements OnInit {
       'grade': 1,
       'lastChange': new Date()
     })
-
     sender.addRow(this.formGroup);
   }
 
@@ -176,11 +158,11 @@ export class TeacherMarksComponent implements OnInit {
         console.log(course);
       });;
     }
+    this.reflesh();
     this.closeEditor(sender);
     sender.closeRow(rowIndex);
-    this.reflesh(rowIndex);
   }
-  reflesh(rowIndex) {
+  reflesh() {
     this.teacherMarksService.getStudentsOfGivenClassById(this.selectedClassId).subscribe(res => {
       this.studentsList = res;
       this.teacherMarksService.getStudentsGrades(this.selectedClassId, this.selectedTeacherCourseId).subscribe(res => {
@@ -188,27 +170,19 @@ export class TeacherMarksComponent implements OnInit {
         console.log(this.studentsMarkGet);
         this.mapGradesToString();
         this.studentsList.forEach(e => console.log(e.grades));
-        this.selectedStudentObject = <StudentMarksGetDTODisplay>this.studentsMarkGet[rowIndex];
+        this.selectedStudentObject = <StudentMarksGetDTODisplay>this.studentsMarkGet[this.selectedStudentRowNumber];
         this.selectedStudentObject.grades.forEach(e => {
           e.grade = this.mapGradeToStr[e.enumGrade];
           console.log(e.grade);
         });
       }, () => { alert("Couldnt fetch student marks") })
-      //this.teacherCoursesList.forEach(e=> e.displayName = e.course.name);
     }, () => { alert("Couldnt fetch stundents") })
 
   }
-  //   sender.closeRow(rowIndex);
-  // }
-
-  // public removeHandler({ dataItem }) {
-  //   this.coursesService.deleteCourse(dataItem).subscribe(resp=>{
-  //     console.log(resp);
-  //   });
-  // }
 
   private closeEditor(grid, rowIndex = this.editedRowIndex) {
     grid.closeRow(rowIndex);
+    console.log(this.editedRowIndex);
     this.editedRowIndex = undefined;
     this.formGroup = undefined;
   }
@@ -216,6 +190,6 @@ export class TeacherMarksComponent implements OnInit {
     this.teacherMarksService.deleteMark(dataItem.id).subscribe(resp => {
       console.log(resp);
     });
-    this.reflesh(rowIndex);
+    this.reflesh();
   }
 }
